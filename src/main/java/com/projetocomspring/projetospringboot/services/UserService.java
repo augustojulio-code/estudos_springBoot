a365f146fb54dc30d0a80cc83c2a23843024ec3b
Julio <julio.mech@hotmail.com>
2023-04-25 09:41:07 -0300

    Exception handling update

--- src/main/java/com/projetocomspring/projetospringboot/services/UserService.java
+++ src/main/java/com/projetocomspring/projetospringboot/services/UserService.java
@@ -14,6 +14,8 @@ import com.projetocomspring.projetospringboot.repositories.UserRepository;
 import com.projetocomspring.projetospringboot.services.exceptions.DataBaseExceptions;
 import com.projetocomspring.projetospringboot.services.exceptions.ResourceNotFoundExceptions;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -53,9 +55,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundExceptions(id);
+		}
 	}
 
 
